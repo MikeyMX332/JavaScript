3499a8504b62b99755929826031d812dae4b6dd3
MikeyMX332 <163356135+MikeyMX332@users.noreply.github.com>
2024-09-18 13:04:43 -0600

    Tipos de datos

---
+++ 01-Fundamentos/tipos-de-datos.js
@@ -0,0 +1,77 @@
+alert ("Tipos enlazados")
+
+/*
+
+Los tipos de datos se dividen en dos ctegrías: Primitivos y Complejos.
+
+Tipos de datos  primitivos.
+
+1. Datos básicos que no se consideran "objetos" y no tienen "métodos".
+
+Numbre o Número
+
+Representación de valores númericos ya sea enteros o con decimáles (puto flotante).
+
+la forma más recomendada iniciando con minusculs y apartir de la segunda palabra iniciando con mayusculas.
+
+* String o Cadea de Caracteres
+
+Se usa para represenatar texto. El texto se coloca entre comillas dobles o simples "texto".
+
+* Boolean o Boleanos
+
+Estos representan un valor lógico que puede ser true o false.
+
+* Undefined o Indefinido
+
+Es el valor que se le asigna a una variable que ha sido declarada pero no inicializada.
+
+2. Tipos de datos complejos.
+
+* Array o Arreglo
+
+Son un conjunto de datos que los almacena en una lista ordenada de elementos. Los datos dentro de los arregls se escriben entre corchetes  [ ] y separadps por comas ,
+
+*/ 
+
+// Number //
+let miNumero = 7
+let miNumeroDecimal = 5.766
+
+console. log (miNumero);
+console. log (miNumeroDecimal);
+
+// Strings //
+
+let miNombre = "Miguel";
+let miApellido = 'Ayala';
+
+console.log (miNombre);
+console.log (miApellido);
+
+// Boolean //
+
+let miVerdad = true;
+let miMentira = false;
+
+console.log (miVerdad);
+console.log (miMentira);
+
+// Undefined //
+
+let x ;  // Declarar una variable sin inicializar //
+
+console.log (x);  //Valor que el usuario introduzca//
+
+// Array //
+
+let misFrutasFavoritas = ["Mango", "Uvas", "Sandia"];
+let miNumerosSuerte = ["7", "117", "1888"];
+let otroArray = ["Animal", true, 18];
+
+console.log (misFrutasFavoritas);
+console.log (miNumerosSuerte);
+console.log (otroArray);
+
+
+
