98e6eea668059066d58599e02d4f8056d94410ea
MikeyMX332 <163356135+MikeyMX332@users.noreply.github.com>
2024-09-23 13:48:04 -0600

    Aprendimos operadores aritmeticos

---
+++ 01-Fundamentos/operadores.js
@@ -0,0 +1,61 @@
+alert ("operadores")
+
+/* 
+*Operadores
+
+Son símbolos utilizados para realizar operaciones sobre valores y variables-
+
+1. Ariméticos
+2. Asignación
+3. Comparación
+
+*/
+
+/* 
+1. Operadores aritmeticos
+
+Se utilizan para realizar cálculos matemáticos
+
+*/
+
+// suma (+): Suma de dos valores
+
+let suma = 16 + 6; //22
+
+console.log (suma); 
+
+// Resta (-): Resta el segundo valor al primero
+let resta = 54 - 15; //39
+console.log(resta)
+
+//Multiplicación (*): Multiplica los dos valores
+let multi = 4 *8; //32
+console.log (multi)
+
+// División (/) divide el primer valor por el seguno.
+let div= 20 / 4; //5
+console.log (div);
+
+//Módulo (%): Devuelve el resto de la división entre dos valores.
+let mod = 7 % 2; //1
+let mod2 = 9 % 3; //0
+console.log (mod2)
+console.log (mod);
+
+// Exponenciación (**): Elev un numero  la potencia de otro.
+let expo = 3**2; // 9
+let expo2 = 5**12; // 
+console.log(expo);
+console.log(expo2);
+
+// Incremento (++) Incrementa el valor en 1.
+
+let x = 2;
+x++;
+console.log(x);
+
+// Decremento (--): Decrementa el valor en 1.
+
+let y= 6;
+y--; //5
+console.log(y)
