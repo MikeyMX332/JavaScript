4f103ee0d11a03173f64d6f92f7ade6545c7f36c
MikeyMX332 <163356135+MikeyMX332@users.noreply.github.com>
2024-09-25 13:04:14 -0600

    Aprendimos operadores de asignacion y comparacion

--- 01-Fundamentos/operadores.js
+++ 01-Fundamentos/operadores.js
@@ -59,3 +59,90 @@ console.log(x);
 let y= 6;
 y--; //5
 console.log(y)
+
+/* Operdoress de Asignacion
+
+Estos operadores asignan valores a la variables.
+
+*/
+
+// Asignacion (=) Asigna un valor  a una variable.
+
+let w = 45;
+
+//Asignacion con suma (+=): Suma el valor a una varuable y la reasigna.
+
+w+= 5; // 50
+console.log(w)
+
+//Asignacion con resta (-=): Resta el valor a una variable y la resigna.
+
+w -=15 //35
+console.log (w)
+
+//Asignacion con multiplicacion (*=): Multiplica una variable por un valor y lo reasigna.
+
+w *=2 //70
+console.log (w)
+
+//Asignacion con divicion (/=): DIvide una variable por un valor y la reasigna.
+
+w /=10; //7
+console.log (w)
+
+/* 
+3. Comparacion
+
+Estos operadores comparan dos valores y devuelven "true" o "false".
+
+*/
+
+// Igualdad (==): Compara dos valores para ver si son iguales. NOTA: Sin tener en cuenta el tipo de dato.
+let valor = "5";
+ 
+let compara = 5==valor;
+
+console.log (compara) //true
+
+// Igualdad estricta (===): Comparar dos valores y su tipo para ver si son exactamente iguales. NOTA: No toma el mismo tipo de dato.
+
+compara = 5 === valor;
+console.log (compara) //false
+
+// Desigualdad: (!=): Verificar si dos valores no son iguales.
+
+let valor1 = 15;
+let valor2=10;
+//Si dos valores son iguales sale "false" pero si son desiguales saldrá "true"
+
+console.log (valor != valor2)
+
+//Desigualdad estricta: (==!) Verifica dos valores y sus tipos no son exactamente iguales
+
+valor1 ="10"
+valor2= 10
+
+console.log(valor1 !== valor2) //true
+
+//Mayor que (>): Verifica si el primer valor es mayor que el segundo.
+
+compara = 10>5
+console.log (compara) //true
+//NOTA: Si invertimos los valores saldra "false" 
+
+//Mayor o igual que (>=): Verificar si le primer valor es mayor o igual que el segundo
+compara = 10 >=5;
+console.log(compara) //true
+//Nota si ponemos 5>=10 saldra "false" pero si ponemos 5>=5 saldra "true" porque son iguales
+
+//Menor que (<): Verifica el primer valor es menor ue el segundo.
+
+console.log (3<8); //true
+console.log (10<8); // false
+console.log("5"< 5); //false
+
+// menor o igual que (<=): Verifica si el primer valor es menor o igual que el segundo valor.
+console.log(4 <= 10); //true
+console.log(20 <= 100) //true
+console.log(45 <=7) //false
+console.log(8<=8) //true
