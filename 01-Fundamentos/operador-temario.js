fca9b5baf24825bcb96e137115e511d2fafc9f97
MikeyMX332 <163356135+MikeyMX332@users.noreply.github.com>
2024-10-14 14:00:30 -0600

    Aprendimos operadores ternarios!

---
+++ 01-Fundamentos/operador-temario.js
@@ -0,0 +1,34 @@
+/* 
+<<<ejemplo condicionales if y else
+1. Declarar una variable edad y le asignamos un valor numerico
+2. Comprobamos si edad es igual o mayor a 18. ,pstra,ps eñ ,emsake "Eres mayor de edad :3".
+3. Si edad no es mayor que 18, mostramos el mensaje "eres un bebé xD"
+*/
+
+/* let edad= 22;
+if ( edad >=18) {
+  console.log ("Eres mayor de edad! :3");
+} else {
+    console.log ("Eres un bebé xD");
+  } */
+
+  /* 
+  El operador temario es una forma másconcisa de escribir una condicion if / else
+
+  Trabaja con tres operadores:
+
+  1. Una Condición
+  2. La expresion si la condicion es verdadera
+  3. La expresión si la condición es falsa
+
+  Sintaxis
+
+  condición ? expresionSiVerdadera: expresionSiFalsa
+
+  */
+
+  let edad =18;
+
+  console.log (edad >= 18 ? "Eres mayor de edad :3" : "Eres un bebé xD");
+
+  let resultado = edad >= 18 ? "Eres mayor de edad :3" : "Eres un bebé xD";
